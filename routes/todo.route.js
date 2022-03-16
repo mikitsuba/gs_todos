f03e8ecf6f086a26f53c1d98d8aa774b59e04169
t-miki <tsubasa4997@gmail.com>
2022-03-16 20:26:20 +0900

    edited rooting

--- routes/todo.route.js
+++ routes/todo.route.js
@@ -4,3 +4,4 @@ import { readAllTodoData, createTodoData } from "../controllers/todo.controller.
 export const todoRouter = express.Router();
 
 todoRouter.get("/", (req, res) => readAllTodoData(req, res));
+todoRouter.post("/", (req, res) => createTodoData(req, res));
