6ce6a723c0c32ee8708ee96f5d7f2f0434c78d1f
t-miki <tsubasa4997@gmail.com>
2022-03-16 20:43:32 +0900

    added real findAll logic in datalayer

--- repositories/todo.repository.js
+++ repositories/todo.repository.js
@@ -8,9 +8,18 @@ const supabase = createClient(
   process.env.SUPABASE_API_KEY
 );
 
-export const findAll = () => {
-  return { message: "OK" };
-};
+export const findAll = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("todos")
+        .select()
+        .order("deadline", { ascending: true })
+        .order("todo", { ascending: true });
+      return data;
+    } catch (e) {
+      throw Error("Error while getting Todo Data");
+    }
+  };
 
 export const store = async ({ params }) => {
   try {
