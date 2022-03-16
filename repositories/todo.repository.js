75511098b2866f035bcfea55155f2b6553135054
t-miki <tsubasa4997@gmail.com>
2022-03-16 20:29:24 +0900

    connected data layer to todos table.

--- repositories/todo.repository.js
+++ repositories/todo.repository.js
@@ -1,3 +1,27 @@
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_API_KEY
+);
+
 export const findAll = () => {
-    return { message: "OK" };
+  return { message: "OK" };
+};
+
+export const store = async ({ params }) => {
+  try {
+    const { data, error } = await supabase.from("todos").insert([
+      {
+        ...params,
+        is_done: false,
+      },
+    ]);
+    return data;
+  } catch (e) {
+    throw Error("Error while store Todo Data");
+  }
 };
